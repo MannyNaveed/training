5ec0ee6863e77ec3329af38e81d03a0a72402905
MuhammadNaveed(CTR) <muhammad.naveed@asd-web.com>
2021-10-07 12:46:49 -0400

    Adding multiply to calculator

--- src/com/vbms/testproj/naveed/Calculator.java
+++ src/com/vbms/testproj/naveed/Calculator.java
@@ -10,8 +10,11 @@ public class Calculator {
     public int add(int num1,int num2){
         return num1+num2;
     }
-     public int subtract(int num1,int num2){
+    public int subtract(int num1,int num2){
          return  num1-num2;
+     }
+     public int multiply(int num1, int num2){
+        return num1*num2;
      }
 
 
